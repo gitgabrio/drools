65a8473d21dedb7fc58825563b985b867478b30c
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2023-07-26 15:33:05 +0200

    [efesto_communication_layer] Implemented CanManageInputResponseProducer with tests

---
+++ efesto/efesto-kafka/efesto-kafka-runtime/efesto-kafka-runtime-services/src/main/java/org/kie/efesto/kafka/runtime/services/producer/CanManageInputResponseProducer.java
@@ -0,0 +1,71 @@
+package org.kie.efesto.kafka.runtime.services.producer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.kie.efesto.common.api.exceptions.KieEfestoCommonException;
+import org.kie.efesto.kafka.runtime.provider.messages.EfestoKafkaRuntimeCanManageInputResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.kie.efesto.common.core.utils.JSONUtils.getObjectMapper;
+import static org.kie.efesto.kafka.api.KafkaConstants.BOOTSTRAP_SERVERS;
+import static org.kie.efesto.kafka.api.KafkaConstants.RUNTIMESERVICE_CANMANAGEINPUTRESPONSE_TOPIC;
+
+public class CanManageInputResponseProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(CanManageInputResponseProducer.class);
+
+    private static final AtomicLong COUNTER = new AtomicLong();
+
+
+    public static void runProducer(boolean toPublish, long messageId) {
+        logger.info("runProducer");
+        final Producer<Long, JsonNode> producer = createProducer();
+        runProducer(producer, toPublish, messageId);
+    }
+
+    public static void runProducer(final Producer<Long, JsonNode> producer, boolean toPublish, long messageId) {
+        logger.info("runProducer {}", producer);
+        long time = System.currentTimeMillis();
+
+        try {
+            JsonNode jsonNode = getJsonNode(toPublish, messageId);
+            final ProducerRecord<Long, JsonNode> record =
+                    new ProducerRecord<>(RUNTIMESERVICE_CANMANAGEINPUTRESPONSE_TOPIC, COUNTER.incrementAndGet(), jsonNode);
+            RecordMetadata metadata = producer.send(record).get();
+            long elapsedTime = System.currentTimeMillis() - time;
+            logger.info("sent record(key={} value={}) " +
+                            "meta(partition={}, offset={}) time={}\n",
+                    record.key(), record.value(), metadata.partition(),
+                    metadata.offset(), elapsedTime);
+        } catch (Exception e) {
+            throw new KieEfestoCommonException(e);
+        } finally {
+            producer.flush();
+            producer.close();
+        }
+    }
+
+    static JsonNode getJsonNode(boolean toPublish, long messageId) {
+        EfestoKafkaRuntimeCanManageInputResponseMessage responseMessage = new EfestoKafkaRuntimeCanManageInputResponseMessage(toPublish, messageId);
+        return getObjectMapper().valueToTree(responseMessage);
+    }
+
+    private static Producer<Long, JsonNode> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, CanManageInputResponseProducer.class.getSimpleName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+
+}
